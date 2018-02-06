9d298186ccd3f9b6390012ec64e3bc5ca48edd38
Juan Cruz Maschio <36174206+MaschioJuanCruz@users.noreply.github.com>
2018-02-05 22:03:19 -0300

    EntradaSalida de 1 a 5
    Los ejercicios para practicar la entrada y salida de datos

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,19 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	// alert("anda");
+	// Una línea de instrucción termina con punto y coma (;)
+	var nombre;
+	/*
+	alert("nombre");
+	alert(nombre);
+	nombre="Juan";
+	alert(nombre);
+	*/
+	nombre=prompt("ingrese su nombre","natalia natalia");
+	alert("su nombre es: "+nombre);
+	//var person = prompt("Please enter your name", "Harry Potter");
+}
+
+
