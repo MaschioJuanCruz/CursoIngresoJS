90c0cb6bc58a79bb1c0dbad427d974b5e44df55d
Juan Cruz Maschio <36174206+MaschioJuanCruz@users.noreply.github.com>
2018-02-06 22:14:56 -0300

    Clase de 2018-Febrero-06

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,23 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	/* El símbolo % devuelve el resto de una división
+	se llama módulo*/
+
+	//var resultado;
+	//resultado=10%2;
+	//alert(resultado);
+	//resultado=2%10;
+	//alert(resultado);
+
+	var dividendo;
+	var divisor;
+
+	dividendo = document.getElementById('numeroDividendo').value;
+	divisor = document.getElementById('numeroDivisor').value;
+
+
+	// terminar ejercicio
+
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,21 @@ en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
 
-	
+/*
+	var sueldo;
+	var sueldoConAumento;
+	sueldo=100;
+	sueldoConAumento= sueldo*1.1;
+
+	alert(sueldoConAumento)*/
+
+	var sueldo;
+	var sueldoConAumento;
+
+	sueldo = document.getElementById('sueldo').value;
+	sueldo = parseInt(sueldo);
+	//alert(sueldo);
+
+	sueldoConAumento = sueldo*1.1;
+	document.getElementById('resultado').value=sueldoConAumento;
 }
