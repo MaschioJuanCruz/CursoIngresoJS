cccf5a60949bf8ce522dc136f5c700f8fc8bf02c
Juan Cruz Maschio <36174206+MaschioJuanCruz@users.noreply.github.com>
2018-02-13 22:25:02 -0300

    Practica Particular003

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,42 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
-
+	var largoTerreno;
+	var anchoTerreno;
+	var perimetro;
+	var alambre;
+	largoTerreno=document.getElementById('Largo').value;
+	anchoTerreno=document.getElementById('Ancho').value;
+	largoTerreno=parseInt(largoTerreno);
+	anchoTerreno=parseInt(anchoTerreno);
+	perimetro=(largoTerreno*2)+(anchoTerreno*2);
+	alambre=(perimetro*3);
+	alert("Usted necesita "+alambre+" metros para alambrar con 3 hilos");
 }
 function Circulo () 
 {
-	
+	var radio;
+	var perimetro;
+	var alambre;
+	radio=document.getElementById('Radio').value;
+	radio=parseInt(radio);
+	perimetro=(radio*2)*3.14;
+	alambre=perimetro*3;
+	alert("Usted necesita "+alambre+" metros para alambrar con 3 hilos");
 }
 function Materiales () 
 {
-	
+	var largoTerreno;
+	var anchoTerreno;
+	var superficie;
+	var cemento;
+	var cal;
+	largoTerreno=document.getElementById('Largo').value;
+	anchoTerreno=document.getElementById('Ancho').value;
+	largoTerreno=parseInt(largoTerreno);
+	anchoTerreno=parseInt(anchoTerreno);
+	superficie=largoTerreno*anchoTerreno;
+	cemento=superficie*2;
+	cal=superficie*3;
+	alert("Usted necesita "+cemento+" bolsas de cemento para el contrapiso y "+cal+" bolsas de cal para el contrapiso");
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,50 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+var precio1;
+var precio2;
+var precio3;
+var suma;
+precio1=document.getElementById('PrecioUno').value;
+precio2=document.getElementById('PrecioDos').value;
+precio3=document.getElementById('PrecioTres').value;
+precio1=parseInt(precio1);
+precio2=parseInt(precio2);
+precio3=parseInt(precio3);
+suma=precio1+precio2+precio3;
+alert("La suma de los tres precios es igual a: "+suma);
 }
 function Promedio () 
 {
-	
+var precio1;
+var precio2;
+var precio3;
+var promedio;
+precio1=document.getElementById('PrecioUno').value;
+precio2=document.getElementById('PrecioDos').value;
+precio3=document.getElementById('PrecioTres').value;
+precio1=parseInt(precio1);
+precio2=parseInt(precio2);
+precio3=parseInt(precio3);
+promedio=(precio1+precio2+precio3)/3;
+alert("El promedio de los tres precios es igual a: "+promedio);
 }
 function PrecioFinal () 
 {
-	
+var precio1;
+var precio2;
+var precio3;
+var suma;
+var iva;
+var precioFinal;
+precio1=document.getElementById('PrecioUno').value;
+precio2=document.getElementById('PrecioDos').value;
+precio3=document.getElementById('PrecioTres').value;
+precio1=parseInt(precio1);
+precio2=parseInt(precio2);
+precio3=parseInt(precio3);
+suma=precio1+precio2+precio3;
+iva = suma/100*21
+precioFinal=suma+iva;
+alert("El promedio de los tres precios es igual a: "+precioFinal);	
 }
