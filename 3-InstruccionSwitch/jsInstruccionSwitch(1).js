3b82ecca797cf706e4c78d182563525335d58163
Juan Cruz Maschio <36174206+MaschioJuanCruz@users.noreply.github.com>
2018-02-17 10:38:35 -0300

    Clase 27-Febrero

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,8 +1,26 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
 
+var mesDelAño = document.getElementById('mes').value;
+switch(mesDelAño)
+{
+	case "Febrero" :
+		alert("28 o 29 dias");
+		break;
+	case "Enero" :
+		alert("Que comiences bien el año!!!");
+		break;
+	case "Marzo" :
+		alert("A Clases!!!!");
+		break;
+	case "Julio" :
+		alert("Se vienen las vacaciones!!!");
+		break;
+	case "Diciembre" :
+		alert("Felices Fiestas!!!");
+		break;
+ }
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,10 +2,25 @@ function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
-alert (mesDelAño);
-
-
-
-
+switch(mesDelAño)
+{
+	case "Enero":
+	case "Febrero":
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+		alert("Falta para el invierno!!!");
+		break;
+	case "Julio":
+	case "Agosto":
+		alert("Abrigate que hace frío");
+		break;
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+		alert("Ya pasamos el frío, ahora calor!!!");
+		break;
+}
 }//FIN DE LA FUNCIÓN
