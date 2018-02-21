49434a478e56f02bd094fddf2d60a1fc90032f17
Juan Cruz Maschio <36174206+MaschioJuanCruz@users.noreply.github.com>
2018-02-20 22:11:40 -0300

    Clase 20 de Febrero

---
+++ jsIteracionesFor (7).js
@@ -0,0 +1,22 @@
+function Mostrar()
+{
+	var contadorDivisores=0;
+	var numero=prompt("Ingrese un número");
+	numero=parseInt(numero);
+	for(anterior=2; anterior<numero;anterior++)
+	{
+		if(numero%anterior==0)
+		{
+			document.write("<br>"+anterior);
+			contadorDivisores++;
+		}
+		if (anterior>(numero/2))
+			break;
+	}
+	if (contadorDivisores==0)
+	{
+		document.write("Es primo");
+	}
+
+
+}//FIN DE LA FUNCIÓN
