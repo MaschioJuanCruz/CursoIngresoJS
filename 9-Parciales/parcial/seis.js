af80e14a22daaae622d29777c99c805525628a3b
Juan Cruz Maschio <36174206+MaschioJuanCruz@users.noreply.github.com>
2018-02-24 12:20:13 -0300

    Clase del 24 de Febrero 2018

--- 9-Parciales/parcial/seis.js
+++ 9-Parciales/parcial/seis.js
@@ -1,4 +1,30 @@
 function Mostrar()
 {
+var importeDiario;
+var importeMayor;
+var importeMenor;
+var importe;
+importeDiario = 0;
+importeMayor = 0;
+importeMenor = 0;
+importe = 0;
 
+while (importe<24)
+{
+	importeDiario=prompt("Ingrese el monto del día"+importe);
+	importeDiario=parseInt(importeDiario);
+		if (importeDiario >0)
+		{
+			if (importeDiario<importeMayor)
+			{
+				importeMayor = importeDiario;
+			}
+			if (importeDiario>importeMenor)
+			{
+				importeMenor = importeDiario;
+			}
+		}
+	importe=importe++
+}
+document.write("El importe Mayor es :"+importeMayor+"<br> El importe Menor es : "+importeMenor);
 }
