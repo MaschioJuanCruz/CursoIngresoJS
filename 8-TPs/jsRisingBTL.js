b70191d11ce6ea89c641489834c656bc13252759
Juan Cruz Maschio <36174206+MaschioJuanCruz@users.noreply.github.com>
2018-02-27 22:07:09 -0300

    Clase del 27 de Febrero

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,79 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+var edad;
+var sexo;
+var estadoCivil;
+var sueldoBruto;
+var numeroDeLegajo;
+var nacionalidad;
+//Ingreso y valido edad
+edad=prompt("Ingrese su edad debe ser entre 18 y 90 años inclusive");
+edad=parseInt(edad);
+	while (edad<18 || edad>90 || isNaN(edad))
+		{
+		edad=prompt("Ingrese su edad debe ser entre 18 y 90 años inclusive");
+		edad=parseInt(edad);
+		}
+//Ingreso y valido sexo
+sexo=prompt("Ingrese su sexo - F = fememnino o M = masculino");
+	while (sexo!="F" && sexo!="M")
+		{
+			sexo=prompt("Ingrese su sexo - F = fememnino o M = masculino");
+		}
+//Ingreso y valido estado civil
+estadoCivil=prompt("Ingrese 1 si es Soltero, 2 si es Casado, 3 si es Divorsiado o 4 si es Viudo");
+estadoCivil=parseInt(estadoCivil);
+	while (estadoCivil!=1 && estadoCivil!=2 && estadoCivil!=3 && estadoCivil!=4)
+		{
+		estadoCivil=prompt("Ingrese 1 si es Soltero, 2 si es Casado, 3 si es Divorsiado o 4 si es Viudo");
+		estadoCivil=parseInt(estadoCivil);
+		}
+		if (estadoCivil=1)
+		{
+			estadoCivil="Soltero"
+		}else if (estadoCivil=2)
+			{
+				estadoCivil="Casado"
+			}
+		else if (estadoCivil=3)
+		{
+			estadoCivil="divorciado"
+		}
+		else
+		{
+			estadoCivil="Viudo"
+		}
+	
+//Ingreso y valido Sueldo Bruto
+	sueldoBruto=prompt("Ingrese su sueldo bruto No menor a 8.000 $");
+	sueldoBruto=parseInt(sueldoBruto);
+		while (sueldoBruto<8000 || isNaN(sueldoBruto))
+		{
+		sueldoBruto=prompt("Ingrese su sueldo bruto No menor a 8.000 $");
+		sueldoBruto=parseInt(sueldoBruto);
+		}
+	
+//Ingreso y valido Legajo
+numeroDeLegajo=prompt("ingrese su numero de legajo ");
+numeroDeLegajo=parseInt(numeroDeLegajo);
+	while (numeroDeLegajo<1000 || numeroDeLegajo>9999 || isNaN(numeroDeLegajo))
+	{
+	numeroDeLegajo=prompt("ingrese su numero de legajo ");
+	numeroDeLegajo=parseInt(numeroDeLegajo);
+	}
+
+//Ingreso y valido Nacionalidad
+nacionalidad=prompt("Ingrese su Nacionalidad A para Argentino, E para Extranjero o N para Nacionalizados");
+	while (nacionalidad!=A && nacionalidad!=E && nacionalidad!=N)
+	{
+		nacionalidad=prompt("Ingrese su Nacionalidad A para Argentino, E para Extranjero o N para Nacionalizados");
+	}
+
+document.getElementById('Edad').value=edad;
+document.getElementById('Sexo').value=sexo;
+document.getElementById('EstadoCivil').value=estadoCivil;
+document.getElementById('Sueldo').value=sueldoBruto;
+document.getElementById('Legajo').value=numeroDeLegajo;
+document.getElementById('Nacionalidad').value=nacionalidad;
 }
