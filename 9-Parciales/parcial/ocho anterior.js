a799d1f036505a39f954e79c47cce377e2ad7ee9
Juan Cruz Maschio <36174206+MaschioJuanCruz@users.noreply.github.com>
2018-02-27 22:08:07 -0300

    Clase del 27 de Febrero

---
+++ 9-Parciales/parcial/ocho anterior.js
@@ -0,0 +1,109 @@
+/*
+Ingreso de alumnos, no sabemos cuantos, pedir nombre,
+y mostrar al final cuantos alumnos ingresaron.
+*/
+/*
+En este archivo conseguir cargar los datos de la edad
+el promedio total, el nombre de la mejor nota y de la peor
+Los promedios de notas y de edades, cantidad de personas
+por sexo y buscar varios tipos de combinacion de datos.
+*/
+function Mostrar()
+{
+	var nombre;
+	var nota;
+	var contador;
+	var promedio;
+	var seguir;
+	var acumuladorDeNotas;
+	var sexo;
+	var contadorDeMujeres;
+	var contadorDeVarones;
+	var varonesDesaprobados;
+	var MujeresDesaprobadas;
+
+	acumuladorDeNotas=0;
+	seguir="";
+	contador=0;
+	promedio=0;
+	acumuladorDeNotas=0;
+	contadorDeMujeres=0;
+	contadorDeVarones=0;
+	varonesDesaprobados=0;
+	MujeresDesaprobadas=0;
+
+	while (seguir!="no")
+	{
+		contador=contador+1;
+		nombre=prompt("ingrese su nombre :");
+		nota=prompt("Ingrese su nota: ");
+		nota=parseInt(nota);
+		while (nota<0 || nota>10)
+			{
+			nota=prompt("Ingrese su nota: ");
+			nota=parseInt(nota);
+			}
+			sexo=prompt("Ingrese se sexo f o m");
+			while (sexo !="f" && sexo !="m")
+			{
+				sexo=prompt("Ingrese se sexo f o m");
+			}
+			if (sexo=="f")
+				{
+				contadorDeMujeres=contadorDeMujeres+1;
+				}
+			else
+				{
+				contadorDeVarones=contadorDeVarones+1;	
+				}
+		if (nota>4 && sexo=="f")
+		{
+			MujeresDesaprobadas=MujeresDesaprobadas+1;
+		}
+		if (nota<4 && sexo=="m")
+		{
+			varonesDesaprobados=varonesDesaprobados+1;
+		}
+
+		
+		
+		seguir=prompt("Ingrese no para salir");
+		acumuladorDeNotas=acumuladorDeNotas+nota;
+
+	}
+
+	promedio=acumuladorDeNotas/contador;
+	document.write("La cantidad de alumnos es "+contador);
+	document.write("<br>El promedio es :"+promedio);
+	document.write("<br> La cantidad de Mujeres es: "+contadorDeMujeres);
+	document.write("<br> La cantidad de Varones es: "+contadorDeVarones);
+	document.write("<br> Los varones Desaprobados son :"+varonesDesaprobados);
+}
+
+/*
+var nombre;
+var cantidad=0;
+var seguir="";
+var nota;
+var notas;
+var promedio;
+nota=0;
+
+while (seguir != "no")
+{
+	cantidad = cantidad +1;
+	nombre=prompt("Ingrese su nombre");
+	nota=prompt("Ingrese su nota");
+	nota=parseInt(nota);
+	while (nota<0 || nota>10)
+	{
+		nota=prompt("Ingrese su nota");
+		nota=parseInt(nota);
+	}
+	notas=notas+nota;
+	seguir = prompt("no para salir");
+}
+document.write("Son :"+cantidad+" de alumnos");
+promedio=nota/cantidad;
+document.write("El promedio es :"+promedio);
+*/
--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -1,109 +1,74 @@
 /*
-Ingreso de alumnos, no sabemos cuantos, pedir nombre,
-y mostrar al final cuantos alumnos ingresaron.
+el nombre del animal
+pedir el peso del animal mayor a 0
+la temperatura del habitat del animal entre -40 y + 40
+informar cantidad de temmperaturas pares ingresadas
+las imprares ingresadas
+el nombre del animal mas pesado
+el nombre del animal menos pesado
+la cantidad de animales cuyo habitat tiene menos de 0 grados
+el promedio de peso de todos los animnales
+la temperatira maxima y minima ingresada
+*****Agregar un contador para inicializar las asignaciones de los pesos y los nombres****
 */
-/*
-En este archivo conseguir cargar los datos de la edad
-el promedio total, el nombre de la mejor nota y de la peor
-Los promedios de notas y de edades, cantidad de personas
-por sexo y buscar varios tipos de combinacion de datos.
-*/
+
+
 function Mostrar()
 {
-	var nombre;
-	var nota;
-	var contador;
-	var promedio;
-	var seguir;
-	var acumuladorDeNotas;
-	var sexo;
-	var contadorDeMujeres;
-	var contadorDeVarones;
-	var varonesDesaprobados;
-	var MujeresDesaprobadas;
+var contador;
+var salir;
+var animal;
+var pesoAnimal;
+var temperaturaHabitat;
+var tempPares;
+var tempImprares;
+var nombrepesado;
+var nombreLiviano;
+var masPesado;
+var menosPesado;
+var menosDeCero;
+var promedioDePeso;
+var tempMaxima;
+var tempMinima;
+salir="no";
 
-	acumuladorDeNotas=0;
-	seguir="";
-	contador=0;
-	promedio=0;
-	acumuladorDeNotas=0;
-	contadorDeMujeres=0;
-	contadorDeVarones=0;
-	varonesDesaprobados=0;
-	MujeresDesaprobadas=0;
-
-	while (seguir!="no")
-	{
-		contador=contador+1;
-		nombre=prompt("ingrese su nombre :");
-		nota=prompt("Ingrese su nota: ");
-		nota=parseInt(nota);
-		while (nota<0 || nota>10)
-			{
-			nota=prompt("Ingrese su nota: ");
-			nota=parseInt(nota);
-			}
-			sexo=prompt("Ingrese se sexo f o m");
-			while (sexo !="f" && sexo !="m")
-			{
-				sexo=prompt("Ingrese se sexo f o m");
-			}
-			if (sexo=="f")
-				{
-				contadorDeMujeres=contadorDeMujeres+1;
-				}
-			else
-				{
-				contadorDeVarones=contadorDeVarones+1;	
-				}
-		if (nota>4 && sexo=="f")
-		{
-			MujeresDesaprobadas=MujeresDesaprobadas+1;
-		}
-		if (nota<4 && sexo=="m")
-		{
-			varonesDesaprobados=varonesDesaprobados+1;
-		}
-
-		
-		
-		seguir=prompt("Ingrese no para salir");
-		acumuladorDeNotas=acumuladorDeNotas+nota;
-
-	}
-
-	promedio=acumuladorDeNotas/contador;
-	document.write("La cantidad de alumnos es "+contador);
-	document.write("<br>El promedio es :"+promedio);
-	document.write("<br> La cantidad de Mujeres es: "+contadorDeMujeres);
-	document.write("<br> La cantidad de Varones es: "+contadorDeVarones);
-	document.write("<br> Los varones Desaprobados son :"+varonesDesaprobados);
-}
-
-/*
-var nombre;
-var cantidad=0;
-var seguir="";
-var nota;
-var notas;
-var promedio;
-nota=0;
-
-while (seguir != "no")
+while (salir!="si")
 {
-	cantidad = cantidad +1;
-	nombre=prompt("Ingrese su nombre");
-	nota=prompt("Ingrese su nota");
-	nota=parseInt(nota);
-	while (nota<0 || nota>10)
-	{
-		nota=prompt("Ingrese su nota");
-		nota=parseInt(nota);
-	}
-	notas=notas+nota;
-	seguir = prompt("no para salir");
+	//Pido nombre del animal
+	animal=prompt("Ingrese el nombre del animal");
+	//Pido y parseo el peso
+	pesoAnimal=prompt("Ingrese el peso del animal");
+	pesoAnimal=parseInt(pesoAnimal)
+;		while (pesoAnimal<=0 || isNaN(pesoAnimal))
+		{
+			pesoAnimal=prompt("Ingrese el peso del animal");
+			pesoAnimal=parseInt(pesoAnimal);
+		}
+		//Comparo el peso máximo
+		if (pesoAnimal>masPesado)
+		{
+			masPesado=pesoAnimal;
+			nombrepesado=animal;
+		}
+	//Comparo el peso minimo
+		if (pesoAnimal<menosPesado)
+		{
+			menosPesado=pesoAnimal;
+			nombreLiviano=animal;
+		}
+	//Pido y parseo la tempteratura
+	temperaturaHabitat=prompt("Ingrese la temperatura del Habitat entre -40 y 40 grados");
+	temperaturaHabitat=parseInt(temperaturaHabitat);
+		while (temperaturaHabitat<-40 || temperaturaHabitat>40 || isNaN(temperaturaHabitat))
+			{
+			temperaturaHabitat=prompt("Ingrese la temperatura del Habitat entre -40 y 40 grados");
+			temperaturaHabitat=parseInt(temperaturaHabitat);
+			}
+	//Pregunto si quiere salir
+	salir=prompt("Desea Salir escriba si/no");
 }
-document.write("Son :"+cantidad+" de alumnos");
-promedio=nota/cantidad;
-document.write("El promedio es :"+promedio);
-*/
+	document.write("El animal mas pesado es :"+nombrepesado);
+	document.write(" Con un peso de : "+masPesado);
+	document.write("<br>El animal mas liviano es :"+nombreLiviano);
+	document.write("con un peso de : "+menosPesado);
+}
